54fab0ffcb0204e968a539e8df05c6f19b7502c1
shivam_raj <shivamraj81020051@gmail.com>
2026-07-30 03:09:46 +0530

    login page added

--- models/User.js
+++ models/User.js
@@ -1,3 +1,12 @@
+// const mongoose = require("mongoose");
+
+// const userSchema = new mongoose.Schema({
+//   email: { type: String, required: true, unique: true },
+//   password: { type: String, required: true }
+// });
+
+// module.exports = mongoose.model("User", userSchema);
+
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
@@ -6,20 +15,3 @@ const userSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("User", userSchema);
-
-
-// const mongoose = require("mongoose");
-
-// const userSchema = new mongoose.Schema({
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     password: {
-//         type: String,
-//         required: true
-//     }
-// });
-
-// module.exports = mongoose.model("User", userSchema);
