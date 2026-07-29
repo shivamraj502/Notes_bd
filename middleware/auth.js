2f65037df34ae1a8b21076afcc5adb32bab0f572
shivam_raj <shivamraj81020051@gmail.com>
2026-07-30 02:22:42 +0530

    code reversed to original

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,21 @@
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
-function authMiddleware(req, res, next) {
-  const authHeader = req.headers.authorization;
+// function authMiddleware(req, res, next) {
+//   const authHeader = req.headers.authorization;
 
-  if (!authHeader) {
-    return res.status(401).json({ message: "No token provided" });
-  }
+//   if (!authHeader) {
+//     return res.status(401).json({ message: "No token provided" });
+//   }
 
-  const token = authHeader.split(" ")[1];
+//   const token = authHeader.split(" ")[1];
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.userId;
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
-}
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.userId = decoded.userId;
+//     next();
+//   } catch (error) {
+//     return res.status(401).json({ message: "Invalid token" });
+//   }
+// }
 
-module.exports = authMiddleware;
+// module.exports = authMiddleware;
